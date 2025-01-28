320a88b5178daeaa9388566a5377d8534aeee07a
SARFO KING MARKESERDA OSEI <king.osei@isspilimbergo.it>
2025-01-28 09:12:58 +0000

    fixed

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -1,41 +1,39 @@
 import { NextRequest, NextResponse, userAgent } from "next/server";
 
+export async function GET(req: NextRequest) {
+  // valori nell header aka contiene valori di info del dispositivo ecc...
 
-export async function GET(req: NextRequest, res: NextResponse) {
+  const getTheHeaders = userAgent(req);
 
-    // valori nell header aka contiene valori di info del dispositivo ecc...
+  const ipAdress = req.headers.get("x-real-ip"); // presa dell'ip
 
-    const getTheHeaders = userAgent(req);
+  const ipEpas = await fetch("https://ip-api.com/json/" + ipAdress); // fetch dell'ip
 
-    const ipAdress = req.headers.get("x-real-ip"); // presa dell'ip
+  const ip = await ipEpas.json();
 
-    const ipEpas = (await fetch("https://ip-api.com/json/"+ipAdress)); // fetch dell'ip
+  const time = new Date().toISOString(); // data e ora
 
-    const ip = await ipEpas.json()
+  await console.log({ getTheHeaders, ip, time }); // log dei dati
 
+  //redirect to homepage
+  const imageUrl = new URL(
+    "/immagine.png",
+    process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"
+  );
+  const imageResponse = await fetch(imageUrl);
 
-    const time = new Date().toISOString(); // data e ora
+  // Check if the image fetch was successful
+  if (!imageResponse.ok) {
+    return new NextResponse("Failed to fetch image", { status: 500 });
+  }
 
-    await console.log({getTheHeaders, ip, time}); // log dei dati
-    
-    
-        //redirect to homepage
-        const imageUrl = new URL("/immagine.png", process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000")
-        const imageResponse = await fetch(imageUrl);
+  // Get the image data as a buffer
+  const imageBuffer = await imageResponse.arrayBuffer();
 
-        // Check if the image fetch was successful
-        if (!imageResponse.ok) {
-            return new NextResponse("Failed to fetch image", { status: 500 });
-        }
-
-        // Get the image data as a buffer
-        const imageBuffer = await imageResponse.arrayBuffer();
-
-        // Create a new response with the image data
-        return new NextResponse(imageBuffer, {
-            headers: {
-                "Content-Type": imageResponse.headers.get("Content-Type") || "image/jpeg",
-            },
-        })
-
+  // Create a new response with the image data
+  return new NextResponse(imageBuffer, {
+    headers: {
+      "Content-Type": imageResponse.headers.get("Content-Type") || "image/jpeg",
+    },
+  });
 }
